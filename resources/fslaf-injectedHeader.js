b44bfa83e10c5fd67cfce957fc3d04ddb2d1bb08
Yaron Koren <yaron57@gmail.com>
2022-06-24 22:32:18 -0400

    Initial version

---
+++ resources/fslaf-injectedHeader.js
@@ -0,0 +1,11 @@
+/* Injected Header Testing 1 
+<script src="https://beta.familysearch.org/hf/hf.js"></script>
+*/
+
+/* Injected Header Testing 2 */
+(function() {
+  var injHF = document.createElement('script');
+  injHF.async = true;
+  injHF.src = 'https://beta.familysearch.org/hf/hf.js';
+  document.getElementsByTagName("head")[0].appendChild(injHF);
+})();
